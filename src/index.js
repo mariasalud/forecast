7e58ba9c206dc0be8858ceba5946dc508639a2b3
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-08-25 15:41:16 -0500

    Added the function forecast JavaScript.

--- src/index.js
+++ src/index.js
@@ -22,6 +22,33 @@ function formatData(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  days.forEach(function (day) {
+  forecastHTML =
+   forecastHTML +
+    `
+    <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+       <i class="fas fa-sun"></i>
+       <div class="weather-forecast-temperatures">
+       <span class="weather-forecast-temperature-max">
+        18° </span>
+         <span class="weather-forecast-temperature-min">
+         12° </span>
+       </div>
+     </div>
+   
+`;
+});
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -87,3 +114,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
