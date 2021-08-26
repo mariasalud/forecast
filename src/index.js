fb8be3b8991366ed5d2b7feaaddfdb76e2a8ddd2
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-08-26 13:12:36 -0500

    Updated the forecast coordinates.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ function formatData(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -47,7 +48,14 @@ function displayForecast() {
      forecastElement.innerHTML = forecastHTML;
    }
 
-
+    function getForecast(coordinates) {
+      console.log(coordinates);
+      let apiKey = "77d7aad521d071522cc04f7e20b8ab63";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+      lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&
+      units=metric`;
+      axios.get(apiUrl).then(displayForecast);
+    }
 
 
 
@@ -70,11 +78,10 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatData(new Date());
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -118,4 +125,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
